90ff7fb69d6e2826f914b041a20bee53d4be64a4
MarkedOliv <olivdev4@gmail.com>
2023-09-17 18:30:41 -0400

    vistas de la barra de búsqueda y de los resultados

---
+++ src/app/maps/components/search-bar/search-bar.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.css'],
+})
+export class SearchBarComponent {
+  private debounceTimer?: NodeJS.Timeout;
+
+  onQueryChange(query: string) {
+
+    if(this.debounceTimer) clearTimeout(this.debounceTimer)
+
+    this.debounceTimer = setTimeout(() => {
+      console.log('Mandar esta query: ', query);
+    }, 500)
+  }
+}
